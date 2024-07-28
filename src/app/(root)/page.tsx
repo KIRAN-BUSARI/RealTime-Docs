246072210c2e09a6dc2471b0beac0578242a573b
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-07-29 00:59:48 +0530

    Add images and icons

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Button className="bg-slate-500" variant={"outline"}>
+        Click Me
+      </Button>
+    </div>
+  );
+};
+
+export default Home;
