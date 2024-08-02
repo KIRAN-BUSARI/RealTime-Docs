43e0886b1e2738253acbb7136aa6fb8638dd2028
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-08-02 20:21:27 +0530

    add docuemnts

--- src/app/(root)/documents/[id]/page.tsx
+++ src/app/(root)/documents/[id]/page.tsx
@@ -1,6 +1,11 @@
+import CollaboratedRoom from "@/components/CollaboratedRoom";
 import React from "react";
 const Document = () => {
-  return <div></div>;
+  return (
+    <main className="flex w-full flex-col items-center">
+      <CollaboratedRoom />
+    </main>
+  );
 };
 
 export default Document;
