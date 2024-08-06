5f1ed4f52777056cd895e3513867ed75bb1b9c1a
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-08-06 10:34:36 +0530

    name fix

--- src/components/ActiveCollaboraters.tsx
+++
@@ -1,9 +0,0 @@
-import { useOthers } from "@liveblocks/react/suspense";
-import React from "react";
-
-const ActiveCollaboraters = () => {
-  const others = useOthers();
-  return <div className="">ActiveCollaboraters</div>;
-};
-
-export default ActiveCollaboraters;
