c89c962aaa36efadb4d49ddc36d8494330d6f793
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-08-08 23:40:37 +0530

    fix few bugs

--- src/app/(root)/documents/[id]/page.tsx
+++ src/app/(root)/documents/[id]/page.tsx
@@ -8,7 +8,7 @@ const Document = async ({ params: { id } }: SearchParamProps) => {
 
   if (!clerkUser) redirect("/sign-in");
 
-  const room = getDocument({
+  const room = await getDocument({
     roomId: id,
     userId: clerkUser.emailAddresses[0].emailAddress,
   });
