3143e0e9e68227c8d5751f5480577594fef40529
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-08-17 02:04:20 +0530

    Add notificaions to the project

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -9,6 +9,7 @@ import { dateConverter } from "@/lib/utils";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { Notifications } from "@/components/Notifications";
 
 const Home = async () => {
   const clerkUser = await currentUser();
@@ -24,7 +25,7 @@ const Home = async () => {
     <main className="home-container">
       <Header className="sticky left-0 top-0">
         <div className="flex items-center gap-2 lg:gap-4">
-          Notification
+          <Notifications />
           <SignedIn>
             <UserButton />
           </SignedIn>
