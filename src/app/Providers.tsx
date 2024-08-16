623217ecf654557a562850df64809d8ce28e4cf3
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-08-16 20:13:52 +0530

    Add floating comment and few fixes

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,19 +1,30 @@
 "use client";
 
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 import {
   ClientSideSuspense,
   LiveblocksProvider,
 } from "@liveblocks/react/suspense";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  const { user: clerkUser } = useUser();
   return (
     <LiveblocksProvider
       authEndpoint={"/api/liveblocks-auth"}
       resolveUsers={async ({ userIds }) => {
         const users = await getClerkUsers({ userIds });
         return users;
+      }}
+      resolveMentionSuggestions={async ({ roomId, text }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          text,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+        });
+        // Return a list of users that match the search text
+        return roomUsers;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { clerkClient } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
   try {
@@ -21,6 +22,36 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
     );
 
     return parseStringify(sortedUsers);
+  } catch (error) {
+    console.log(`Error fetching users ${error}`);
+  }
+};
+
+export const getDocumentUsers = async ({
+  roomId,
+  text,
+  currentUser,
+}: {
+  roomId: string;
+  text: string;
+  currentUser: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const roomUsers = Object.keys(room.usersAccesses).filter(
+      (email) => email !== currentUser
+    );
+    if (text.length) {
+      const lowerText = text.toLowerCase();
+
+      const filteredUsers = roomUsers.filter((email: string) =>
+        email.toLowerCase().includes(lowerText)
+      );
+
+      return parseStringify(filteredUsers);
+    }
+    return parseStringify(roomUsers);
   } catch (error) {
     console.log(`Error fetching users ${error}`);
   }
