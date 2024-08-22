c7f2d1c2cdcb55744b4f7c8595db89868f65697a
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-08-22 16:01:33 +0530

    Add edge

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+export const runtime = "edge";
 
 const notFound = () => {
-  return <div>Page not found</div>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      Page not found
+    </div>
+  );
 };
 
 export default notFound;
