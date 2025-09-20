04fbb1559a06a3dde275ba724eca880a447d49a8
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2025-09-20 14:43:03 +0530

    update and fix

---
+++ src/app/_not-found/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+export const runtime = "edge";
+
+const page = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Page not found</p>
+    </div>
+  );
+};
+
+export default page;
